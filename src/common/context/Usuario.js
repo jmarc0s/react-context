7daa9fffa6a7788d39be9a5307b8354896230a07
jmarc0s <joaomarcoscacula88@gmail.com>
2023-10-14 19:35:22 -0300

    Criando contexto de carrinho

--- src/common/context/Usuario.js
+++ src/common/context/Usuario.js
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 
 export const UsuarioContext = createContext();
-
+UsuarioContext.displayName="Usuario"
 export const UsuarioProvider = ({children}) => {
     const [nome, setNome] = useState("")
     const [saldo, setSaldo] = useState(0)
--- src/routes.js
+++ src/routes.js
@@ -3,24 +3,32 @@ import Feira from 'pages/Feira';
 import Login from 'pages/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { UsuarioProvider } from 'common/context/Usuario';
+import { CarrinhoProvider } from 'common/context/Carrinho';
+
 
 function Router() {
 
 
     return (
         <BrowserRouter>
-        
-            <Routes>
-                <Route exact path="/" element={
-                    <UsuarioProvider >
-                        <Login />
-                    </UsuarioProvider>
-                }
-                />
-                <Route path="/feira" element={<Feira />} />
-                <Route path="/carrinho" element={<Carrinho />} />
-            </Routes>
-           
+            <UsuarioProvider >
+                <Routes>
+
+                    <Route exact path="/" element={<Login />} />
+
+
+                    <Route path="/feira" element={
+                        <CarrinhoProvider>
+                        <Feira />
+                        </CarrinhoProvider>
+                    } />
+
+
+                    <Route path="/carrinho" element={<Carrinho />} />
+
+
+                </Routes>
+            </UsuarioProvider>
         </BrowserRouter>
     )
 }
