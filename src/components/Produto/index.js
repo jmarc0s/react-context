8f00ddd48aeb710e8aba6d3bf3f9f3b59bdf4dc1
jmarc0s <joaomarcoscacula88@gmail.com>
2023-10-14 23:50:33 -0300

    Adicionando produto

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -13,29 +13,47 @@ function Produto({
   unidade
 }) {
 
-  const {carrinho, setCarrinho} = useContext(CarrinhoContext);
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  function adicionarProduto(novoProduto){
+    const temOProduto = carrinho.some(itemDoCarrinho => itemDoCarrinho.id === novoProduto.id)
+
+    if(!temOProduto){
+      novoProduto.quantidade = 1;
+      return setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto])
+    }
+
+    setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDocarrinho => {
+      if (itemDocarrinho.id === novoProduto.id) 
+        itemDocarrinho.quantidade += 1;
+      return itemDocarrinho;
+    }))
+    
+    
+    
+  }
   return (
-      <Container>
-        <div>
-          <img
-            src={`/assets/${foto}.png`}
-            alt={`foto de ${nome}`}
-          />
-          <p>
-            {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
-          </p>
-        </div>
-        <div>
-          <IconButton
-            color="secondary"
-          >
-            <RemoveIcon />
-          </IconButton>
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-        </div>
-      </Container>
+    <Container>
+      <div>
+        <img
+          src={`/assets/${foto}.png`}
+          alt={`foto de ${nome}`}
+        />
+        <p>
+          {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+        </p>
+      </div>
+      <div>
+        <IconButton
+          color="secondary"
+        >
+          <RemoveIcon />
+        </IconButton>
+        <IconButton onClick={() => adicionarProduto({nome, foto, id, valor})}>
+          <AddIcon />
+        </IconButton>
+      </div>
+    </Container>
   )
 }
 
